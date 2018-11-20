586590ba6a1f0eef697e71d3b7e07946dce36502
bebanovich <bebanovich@yahoo.com>
2018-11-20 13:19:55 -0800

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,30 +1,32 @@
-//use precompiled template
-var template = Handlebars.templates['tutorial-template.handlebars'];
-// ajax call to get data
-var jsonData =(function(){
-  var json = null;
-  $ajax({
-    'async': false,
-    'global': false,
-    'type' : "GET",
-    'url': "json/tutorials.json",
-    'dataType': "json",
-    'success': function (data) {
-        json = data;
-    }
+$(document).ready(function(){
+  var template = Handlebars.templates['tutorial-template.handlebars'];
+  // ajax call to get data
+  var jsonData =(function(){
+    var json = null;
+    $ajax({
+      'async': false,
+      'global': false,
+      'type' : "GET",
+      'url': "json/tutorials.json",
+      'dataType': "json",
+      'success': function (data) {
+          json = data;
+      }
+      });
+
+    //Handlebar helper to modify data or display conditionally.
+    // Handlebars.registerHelper("shortcuts")
+    //         tutorialHTML = template(resJSON);
+    //
+    //          $('#my-container').html(tutorialHTML);
+    //
+    // },
+  //apply data to template
+  var result = template(jsonData);
+
+  //replace div html with template data
+  $('#tutorialContent').html(result);
+
   });
-
-  //Handlebar helper to modify data or display conditionally.
-  // Handlebars.registerHelper("shortcuts")
-  //         tutorialHTML = template(resJSON);
-  //
-  //          $('#my-container').html(tutorialHTML);
-  //
-  // },
-//apply data to template
-var result = template(jsonData);
-
-//replace div html with template data
-$('#tutorialContent').html(result);
-
-});
+    });
+//use precompiled template
